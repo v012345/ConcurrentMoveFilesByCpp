cafe8d4d6a3d8dd620caa8b0a3b7f2a4a1649340
Meteor <v138468@gmail.com>
2022-09-27 22:38:27 +0800

    init

--- GenerateFiles/main.cpp
+++ GenerateFiles/main.cpp
@@ -2,8 +2,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
+#include <thread>
+#include <vector>
+#define FILES_COUNT 100000
+#define IS_CONCURRENT true
+
+void spawnFiles(int startIndex, int endIndex)
+{
+    std::ofstream myfile;
+    for (size_t i = startIndex; i < endIndex; i++)
+    {
+        myfile.open(std::to_string(i) + ".txt");
+        for (size_t i = 0; i < 5000; i++)
+        {
+            myfile << "Writing this to a file. Writing this to a file. Writing this to a file. Writing this to a file.\n";
+        }
+        myfile.close();
+    }
+}
+
 int main(int argc, char const *argv[])
 {
+    auto start = std::chrono::high_resolution_clock::now();
     if (std::filesystem::exists(std::filesystem::current_path().append("source")))
     {
         std::filesystem::remove_all(std::filesystem::current_path().append("source"));
@@ -16,14 +37,27 @@ int main(int argc, char const *argv[])
     std::filesystem::create_directory("target");
 
     std::filesystem::current_path(std::filesystem::current_path().append("source"));
-    std::ofstream myfile;
+    int concurrent_count = std::thread::hardware_concurrency();
 
-    for (size_t i = 0; i < 100000; i++)
+    std::vector<std::thread> spawnFilesThread;
+
+    if (IS_CONCURRENT)
     {
-        myfile.open(std::to_string(i) + ".txt");
-        myfile << "Writing this to a file.\n";
-        myfile.close();
+        for (size_t i = 0; i < concurrent_count; i++)
+        {
+            spawnFilesThread.push_back(std::thread(spawnFiles, i * (FILES_COUNT / concurrent_count), (i + 1) * (FILES_COUNT / concurrent_count)));
+        }
+        for (auto &&t : spawnFilesThread)
+        {
+            t.join();
+        }
     }
-
+    else
+    {
+        spawnFiles(0, FILES_COUNT);
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "run time " << duration.count() / 1000000 << " s" << std::endl;
     return 0;
 }
--- MoveFiles/main.cpp
+++ MoveFiles/main.cpp
@@ -5,8 +5,23 @@
 #include <chrono>
 #include <string>
 #include <iostream>
+#define IS_CONCURRENT true
+
+std::vector<std::string> all_files;
+
+void moveFiles(int startIndex, int endIndex)
+{
+
+    for (size_t i = startIndex; i < endIndex; i++)
+    {
+        std::filesystem::copy(std::filesystem::current_path().append("source").append(all_files[i]), std::filesystem::current_path().append("target").append(all_files[i]), std::filesystem::copy_options::overwrite_existing);
+        std::filesystem::remove(std::filesystem::current_path().append("source").append(all_files[i]));
+    }
+}
+
 int main(int argc, char const *argv[])
 {
+    auto start = std::chrono::high_resolution_clock::now();
     // 文件
     auto source_path = std::filesystem::current_path().append("source");
     auto target_path = std::filesystem::current_path().append("target");
@@ -14,14 +29,36 @@ int main(int argc, char const *argv[])
     // 逻辑核心数
     int concurrent_count = std::thread::hardware_concurrency();
 
-    std::vector<std::string> all_file;
-
     for (auto &&file : std::filesystem::directory_iterator(source_path))
     {
-        all_file.push_back(file.path().string());
+        all_files.push_back(file.path().filename().string());
     }
 
-    std::cout << all_file[0] << std::endl;
+    std::vector<std::thread> moveFilesThread;
+
+    if (IS_CONCURRENT)
+    {
+        for (size_t i = 0; i < concurrent_count; i++)
+        {
+            moveFilesThread.push_back(std::thread(moveFiles, i * (all_files.size() / concurrent_count), (i + 1) * (all_files.size() / concurrent_count)));
+        }
+        for (auto &&t : moveFilesThread)
+        {
+            t.join();
+        }
+    }
+    else
+    {
+        for (auto &&file : all_files)
+        {
+            std::filesystem::copy(std::filesystem::current_path().append("source").append(file), std::filesystem::current_path().append("target").append(file), std::filesystem::copy_options::overwrite_existing);
+            std::filesystem::remove(std::filesystem::current_path().append("source").append(file));
+        }
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "run time " << duration.count() / 1000000 << " s" << std::endl;
 
     return 0;
 }
