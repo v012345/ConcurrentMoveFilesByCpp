cb603fad8084a6bf7ae103979e3bec881f2f9ad4
v138468 <v138468@gmail.com>
2022-09-27 18:43:38 +0800

    init

--- GenerateFiles/main.cpp
+++ GenerateFiles/main.cpp
@@ -1,5 +1,29 @@
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <string>
 int main(int argc, char const *argv[])
 {
-    /* code */
+    if (std::filesystem::exists(std::filesystem::current_path().append("source")))
+    {
+        std::filesystem::remove_all(std::filesystem::current_path().append("source"));
+    }
+    if (std::filesystem::exists(std::filesystem::current_path().append("target")))
+    {
+        std::filesystem::remove_all(std::filesystem::current_path().append("target"));
+    }
+    std::filesystem::create_directory("source");
+    std::filesystem::create_directory("target");
+
+    std::filesystem::current_path(std::filesystem::current_path().append("source"));
+    std::ofstream myfile;
+
+    for (size_t i = 0; i < 100000; i++)
+    {
+        myfile.open(std::to_string(i) + ".txt");
+        myfile << "Writing this to a file.\n";
+        myfile.close();
+    }
+
     return 0;
 }
--- MoveFiles/main.cpp
+++ MoveFiles/main.cpp
@@ -1,5 +1,27 @@
+#include <filesystem>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <chrono>
+#include <string>
+#include <iostream>
 int main(int argc, char const *argv[])
 {
-    /* code */
+    // 文件
+    auto source_path = std::filesystem::current_path().append("source");
+    auto target_path = std::filesystem::current_path().append("target");
+
+    // 逻辑核心数
+    int concurrent_count = std::thread::hardware_concurrency();
+
+    std::vector<std::string> all_file;
+
+    for (auto &&file : std::filesystem::directory_iterator(source_path))
+    {
+        all_file.push_back(file.path().string());
+    }
+
+    std::cout << all_file[0] << std::endl;
+
     return 0;
 }
